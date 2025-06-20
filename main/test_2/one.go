5701ea6592492ec71da6a00c7e8eb5ec6251cdb0
Pedro Vargas <pedrova69@gmail.com>
2025-06-19 21:55:11 -0500

    First Commit

---
+++ main/test_2/one.go
@@ -0,0 +1,10 @@
+components {
+  id: "one"
+  component: "/main/test_2/one.script"
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/main/test_2/two.go\"\n"
+  ""
+}
